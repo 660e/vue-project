67c474fa1b16098d5bc9203d5011df789673a177
Min <solunar@vip.qq.com>
2024-11-05 21:14:35 +0800

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,47 @@ export default [
       // https://typescript-eslint.io/rules/
       '@typescript-eslint/no-empty-function': 'error',
       '@typescript-eslint/no-inferrable-types': 'error',
+
+      // https://eslint.vuejs.org/rules/
+      'vue/attributes-order': [
+        'error',
+        {
+          order: [
+            'DEFINITION', // is, v-is
+            'LIST_RENDERING', // v-for
+            'CONDITIONALS', // v-if, v-else-if, v-else, v-show, v-cloak
+            'TWO_WAY_BINDING', // v-model
+            'RENDER_MODIFIERS', // v-once, v-pre
+            'CONTENT', // v-text, v-html
+            'SLOT', // slot, v-slot
+            'OTHER_DIRECTIVES', // v-custom-directive
+            'ATTR_DYNAMIC', // v-bind:prop, :prop
+            'EVENTS', // @click, v-on
+            'ATTR_STATIC', // prop, custom-prop
+            'GLOBAL', // id
+            'UNIQUE', // ref, key
+            'ATTR_SHORTHAND_BOOL', // boolean-prop
+          ],
+          alphabetical: true,
+        },
+      ],
+      'vue/custom-event-name-casing': ['error', 'kebab-case'],
+      'vue/html-self-closing': ['error', { html: { normal: 'never', void: 'always' } }],
+      'vue/max-attributes-per-line': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-empty-component-block': 'error',
+      'vue/no-static-inline-styles': ['error'],
+      'vue/require-macro-variable-name': [
+        'error',
+        {
+          defineProps: 'props',
+          defineEmits: 'emit',
+          defineSlots: 'slots',
+          useSlots: 'slots',
+          useAttrs: 'attrs',
+        },
+      ],
+      'vue/singleline-html-element-content-newline': 'off',
     },
   },
 ];
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const a = 1;
+
+console.log(a);
+</script>
 
 <template>
   <div>home</div>
