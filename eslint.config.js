035322b1a03aaca692e079a64e5419417be41b73
Min <solunar@vip.qq.com>
2024-11-05 21:00:03 +0800

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -7,13 +7,12 @@ export default [
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
-  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs['flat/recommended'],
   ...vueTsEslintConfig(),
   skipFormatting,
 ];
