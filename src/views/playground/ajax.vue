c65989df91b00f1c07c0a35acc2648a5c8efdc65
Min <solunar@vip.qq.com>
2024-12-13 22:16:00 +0800

    Update

--- src/views/playground/ajax.vue
+++ src/views/playground/ajax.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
-console.log('ajax');
+import axios from 'axios';
+
+const progress = ref(0);
+const inputRef = ref();
+
+const fileChange = (event: Event) => {
+  const file = (event.target as HTMLInputElement).files?.[0];
+
+  if (file) {
+    const fd = new FormData();
+    fd.append('file', file);
+
+    axios
+      .post('https://run.mocky.io/v3/7fa0cba4-08ab-4c56-9b9d-eb354443d6e0', fd, {
+        onUploadProgress: (progressEvent) => {
+          progress.value = progressEvent.progress ?? 0;
+        },
+      })
+      .finally(() => {
+        progress.value = 1;
+      });
+  }
+};
 </script>
 
 <template>
-  <div>ajax</div>
+  <div class="p-4 text-sm leading-none text-neutral-700">
+    <div class="flex items-center gap-2">
+      <input @change="fileChange" class="hidden" type="file" ref="inputRef" />
+      <button @click="inputRef.click()" class="px-4 py-2 rounded duration-300 border border-blue-400 bg-blue-200 hover:bg-blue-300">Upload</button>
+      <span>{{ progress * 100 }}%</span>
+    </div>
+  </div>
 </template>
