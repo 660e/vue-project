8c5f78e5be8c0d02461bc935c64f58e9e95351d8
Min <solunar@vip.qq.com>
2025-01-05 22:09:20 +0800

    Update

--- src/views/_playground/IndexedDB.vue
+++ src/views/_playground/IndexedDB.vue
@@ -1,3 +1,4 @@
 <template>
-  <div></div>
+  <a href="https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API">https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API</a>
+  <a href="https://www.npmjs.com/package/idb">https://www.npmjs.com/package/idb</a>
 </template>
