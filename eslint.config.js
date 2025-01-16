47a49a5b790830b387cd1ececa87e84f556599c6
Min <solunar@vip.qq.com>
2025-01-16 21:46:49 +0800

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,4 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  skipFormatting,
-]
+export default defineConfigWithVueTs(pluginVue.configs['flat/recommended'], vueTsConfigs.recommended);
