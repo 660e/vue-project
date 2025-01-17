1603c4a32de82dafe897df980a1a6a0b126ddea0
Min <solunar@vip.qq.com>
2025-01-17 22:12:25 +0800

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescri
 
 export default defineConfigWithVueTs(pluginVue.configs['flat/recommended'], vueTsConfigs.recommended, {
   rules: {
+    // https://typescript-eslint.io/rules/
+
+    // https://eslint.vuejs.org/rules/
     'vue/html-self-closing': ['error', { html: { normal: 'never', void: 'always' } }],
     'vue/max-attributes-per-line': 'off',
     'vue/multi-word-component-names': 'off',
