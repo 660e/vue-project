23b0de5ef03a0a53e05eb93809f69eb180a5a664
Min <solunar@vip.qq.com>
2025-01-22 22:29:40 +0800

    Update

--- src/views/_example/RandomUser.vue
+++ src/views/_example/RandomUser.vue
@@ -4,6 +4,9 @@ import { randomuser } from '@/apis/_example';
 const data = ref();
 onMounted(async () => {
   data.value = await randomuser();
+  const { results, info } = data.value;
+  console.log(results);
+  console.log(info);
 });
 </script>
 
