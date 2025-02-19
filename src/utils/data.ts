9d44e21de4ccdc02e93580d3d641dc78ca299db1
Min <solunar@vip.qq.com>
2025-02-19 22:10:39 +0800

    Update

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,6 +2,15 @@ export function author() {
   return '明'.charCodeAt(0).toString(16);
 }
 
-export function flattenTree<T>(data: T[]): T[] {
+export function flattenTree<T>(
+  data: T[],
+  options: {
+    childrenKey: keyof T;
+  } = {
+    childrenKey: 'children' as keyof T,
+  },
+): T[] {
+  console.log(options);
+
   return data;
 }
