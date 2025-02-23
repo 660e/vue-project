20cc2e88b6ea44da8d7f6a957213d494bb668a46
Min <solunar@vip.qq.com>
2025-02-23 21:41:58 +0800

    Update

--- src/views/_example/DeepSeek.vue
+++ src/views/_example/DeepSeek.vue
@@ -1,7 +1,41 @@
 <script setup lang="ts">
-const DEEPSEEK_API_KEY = import.meta.env.VITE_DEEPSEEK_API_KEY;
+import axios from 'axios';
+
+const submit = async () => {
+  try {
+    const response = await axios.post(
+      '/chat/completions',
+      {
+        messages: [
+          {
+            content: 'Hi',
+            role: 'user',
+          },
+        ],
+        model: 'deepseek-chat',
+      },
+      {
+        baseURL: 'https://api.deepseek.com',
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_DEEPSEEK_API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  } finally {
+  }
+};
 </script>
 
 <template>
-  <div>{{ DEEPSEEK_API_KEY }}</div>
+  <div class="flex p-4">
+    <div class="flex-1/2">
+      <button @click="submit" class="rounded px-2 py-1 cursor-pointer bg-neutral-200 hover:opacity-80">Submit</button>
+    </div>
+    <div class="flex-1/2"></div>
+  </div>
 </template>
