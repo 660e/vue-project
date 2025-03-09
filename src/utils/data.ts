c8792effd83e4cfbf2503277a3e67c14dac2a9f3
Min <solunar@vip.qq.com>
2025-03-09 20:00:32 +0800

    Update

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,20 +1,13 @@
 import { is } from './index';
 
-function flattenObjectTree<T>(
-  data: T,
-  options: {
-    childrenKey: keyof T;
-  } = {
-    childrenKey: 'children' as keyof T,
-  },
-) {
+function flattenObjectTree<T>(data: T, childrenKey: keyof T) {
   const source = [data];
   const result: T[] = [];
 
   while (source.length) {
     const node = source.shift()!;
-    const children = node[options.childrenKey] || [];
-    delete node[options.childrenKey];
+    const children = node[childrenKey] || [];
+    delete node[childrenKey];
     source.unshift(...(children as T[]));
     result.push(node);
   }
@@ -22,30 +15,16 @@ function flattenObjectTree<T>(
   return result;
 }
 
-function flattenArrayTree<T>(
-  data: T | T[],
-  options: {
-    childrenKey: keyof T;
-  } = {
-    childrenKey: 'children' as keyof T,
-  },
-) {
-  console.log(options);
+function flattenArrayTree<T>(data: T[], childrenKey: keyof T) {
+  console.log(childrenKey);
 
   return data;
 }
 
-export function flattenTree<T>(
-  data: T | T[],
-  options: {
-    childrenKey: keyof T;
-  } = {
-    childrenKey: 'children' as keyof T,
-  },
-) {
+export function flattenTree<T>(data: T | T[], options: { childrenKey: keyof T } = { childrenKey: 'children' as keyof T }) {
   if (is.array(data)) {
-    return flattenArrayTree(data, options);
+    return flattenArrayTree(data, options.childrenKey);
   }
 
-  return flattenObjectTree(data, options);
+  return flattenObjectTree(data, options.childrenKey);
 }
