4bf56308700ccbd5fc200c931d72871b30d35786
Min <solunar@vip.qq.com>
2025-03-11 19:40:30 +0800

    Update

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -19,7 +19,14 @@ function flattenArrayTree<T>(data: T[], childrenKey: keyof T) {
   return data.flatMap((node) => flattenObjectTree(node, childrenKey));
 }
 
-export function flattenTree<T>(data: T | T[], options: { childrenKey: keyof T } = { childrenKey: 'children' as keyof T }) {
+export function flattenTree<T>(
+  data: T | T[],
+  options: {
+    childrenKey: keyof T;
+  } = {
+    childrenKey: 'children' as keyof T,
+  },
+) {
   if (is.array(data)) {
     return flattenArrayTree(data, options.childrenKey);
   }
@@ -27,6 +34,37 @@ export function flattenTree<T>(data: T | T[], options: { childrenKey: keyof T } 
   return flattenObjectTree(data, options.childrenKey);
 }
 
-export function buildTree<T>(data: T[]) {
-  return data;
+export function buildTree<T>(
+  data: T[],
+  options: {
+    childrenKey: string;
+    idKey: keyof T;
+    parentIdKey: keyof T;
+  } = {
+    childrenKey: 'children',
+    idKey: 'id' as keyof T,
+    parentIdKey: 'parentId' as keyof T,
+  },
+) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const map: any = {};
+  const roots: T[] = [];
+
+  data.forEach((item) => {
+    map[item[options.idKey]] = { ...item, [options.childrenKey]: [] };
+  });
+
+  data.forEach((item) => {
+    const node = map[item[options.idKey]];
+    if (item[options.parentIdKey]) {
+      const parent = map[item[options.parentIdKey]];
+      if (parent) {
+        parent[options.childrenKey].push(node);
+      }
+    } else {
+      roots.push(node);
+    }
+  });
+
+  return roots;
 }
