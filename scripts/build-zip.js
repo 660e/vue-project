ea3abb38405a661452528f9c1b3b20717b5c2e36
Min <solunar@vip.qq.com>
2026-03-05 22:59:38 +0800

    Update

---
+++ scripts/build-zip.js
@@ -0,0 +1,9 @@
+import archiver from 'archiver';
+import fs from 'fs';
+
+const output = fs.createWriteStream('dist.zip');
+const archive = archiver('zip', { zlib: { level: 9 } });
+
+archive.pipe(output);
+archive.directory('dist/', false);
+archive.finalize();
