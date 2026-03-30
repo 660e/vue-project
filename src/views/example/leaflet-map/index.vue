8593aafc79576f2417b1b6d51d78cf664d81d771
Min <solunar@vip.qq.com>
2026-03-30 16:25:35 +0800

    Update

--- src/views/example/leaflet-map/index.vue
+++ src/views/example/leaflet-map/index.vue
@@ -7,24 +7,26 @@ const map = ref<L.Map>();
 onMounted(async () => {
   map.value = L.map('map', {
     attributionControl: false,
-    center: [41.75514499999999, 84.94633349999998],
+    // center: [41.75514499999999, 84.94633349999998],
     crs: CRS_4490,
     maxZoom: 22,
     minZoom: 4,
-    zoom: 6,
+    // zoom: 6,
   });
   // baseLayers.forEach((layer) => layer.addTo(map.value!));
 
   fetch('https://geo.datav.aliyun.com/areas_v3/bound/650000.json')
     .then((response) => response.json())
     .then((data) => {
-      L.geoJSON(data, {
+      const provinceLayer = L.geoJSON(data, {
         style: {
           // color: '#38f8ff',
           weight: 3,
           fillOpacity: 0,
         },
       }).addTo(map.value!);
+
+      map.value!.fitBounds(provinceLayer.getBounds());
     });
   fetch('https://geo.datav.aliyun.com/areas_v3/bound/650000_full.json')
     .then((response) => response.json())
