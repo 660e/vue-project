08e1f7ba74edca657fd013d2322606c621c6ef27
Min <solunar@vip.qq.com>
2026-03-30 10:14:36 +0800

    Update

--- src/views/example/WebComponent.vue
+++ src/views/example/WebComponent.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+onMounted(() => {
+  class WebComponentDemo extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadow = this.attachShadow({ mode: 'open' });
+      const wrapper = document.createElement('div');
+
+      const text = this.getAttribute('text') || '-';
+
+      wrapper.innerHTML = `<div>${text}</div>`;
+
+      shadow.appendChild(wrapper);
+    }
+  }
+  customElements.define('web-component-demo', WebComponentDemo);
+});
+</script>
+
 <template>
-  <div>Web Component</div>
+  <web-component-demo class="text-blue-500" text="Web Component Demo" />
 </template>
